8a3d278e90c4b1538f6062dcc9d11a193d7dd097
codinghjh <hanjunhua.123123@gmail.com>
2015-06-28 14:04:38 +0800

    Maximum Product Subarray

--- CPPLeetCode/Maximum Product Subarray .cpp
+++ CPPLeetCode/Maximum Product Subarray .cpp
@@ -1,33 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "Solutions.h"
 
 using namespace std;
 
 int maxProduct(vector<int>& nums) {
 	int length = nums.size();
-	int** dp = new int*[length];
-	for (auto i = 0; i != length; i++)
+	if (length < 1)
+		return 0;
+	if (length == 1)
+		return nums[0];
+	int global = nums[0], max_local = nums[0], min_local = nums[0];
+	for (auto i = 1; i != nums.size(); i++)
 	{
-		dp[i] = new int[length];
-		for (auto j = 0; j != length; j++)
-			dp[i][j] = 0;
+		int copy = max_local;
+		max_local = max(max(nums[i], max_local*nums[i]), min_local*nums[i]);
+		min_local = min(min(nums[i],nums[i]*min_local),nums[i]*copy );
+		global = max(global, max_local);
 	}
-	for (auto i = 0; i != length; i++)
-	{
-		dp[i][i] = nums[i];
-	}
-	for (auto len = 2; len != length; len++)
-		for (auto i = 0; i + len != length; i++)
-		{
-			dp[i][i + len - 1] = dp[i][i + len - 2] * nums[i + len - 1];
-		}
-	int max = 0;
-	for (auto i = 0; i != length; i++)
-		for (auto j = i; j != length; j++)
-		{
-			if (max < dp[i][j])
-				max = dp[i][j];
-		}
-	return max;
+	return global;
 }

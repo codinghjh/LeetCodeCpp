cb928490bcb6aca569dbca7b69431e8ed85670cc
codinghjh <hanjunhua.123123@gmail.com>
2015-06-30 15:39:10 +0800

    Factorial Trailing Zeroes

---
+++ CPPLeetCode/Factorial Trailing Zeroes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Solutions.h"
+
+using namespace std;
+
+int trailingZeroes(int n) {
+	int result = 0;
+	while (n != 0)
+	{
+		result += n / 5;
+		n = n / 5;
+	}
+	return result;
+}

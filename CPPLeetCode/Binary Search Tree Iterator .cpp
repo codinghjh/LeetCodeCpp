2b9ede76f0d7ea245bf2d98b6a2373084f9715a1
codinghjh <hanjunhua.123123@gmail.com>
2015-07-01 15:41:30 +0800

    Binary Search Tree Iterator

---
+++ CPPLeetCode/Binary Search Tree Iterator .cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <set>
+#include "Solutions.h"
+
+using namespace std;
+
+class BSTIterator {
+private:
+	TreeNode* root;
+	queue<int> minValue;
+	stack<TreeNode*> path;
+	void rightTreeValue(TreeNode *rightNode)
+	{
+		stack<TreeNode*> tmpPath;
+		set<TreeNode*> visited;
+		TreeNode* cur = rightNode;
+		tmpPath.push(cur);
+		while (!tmpPath.empty())
+		{
+			while (tmpPath.top()->left != nullptr&&visited.find(tmpPath.top()->left)==visited.end())
+			{
+				visited.insert(tmpPath.top()->left);
+				tmpPath.push(tmpPath.top()->left);
+			}
+			TreeNode* tmp = tmpPath.top();
+			tmpPath.pop();
+			minValue.push(tmp->val);
+			if (tmp->right != nullptr)
+			{
+				visited.insert(tmp->right);
+				tmpPath.push(tmp->right);
+			}
+		}
+	}
+	void initMinvalue()
+	{
+		if (root == nullptr)
+			return;
+		TreeNode* cur = root;
+		while (cur->left != nullptr)
+		{
+			path.push(cur);
+			cur = cur->left;
+		}
+		minValue.push(cur->val);
+		if (cur->right!=nullptr)
+			rightTreeValue(cur->right);
+	}
+public:
+	BSTIterator(TreeNode *root) {
+		if (root == nullptr)
+			return;
+		this->root = root;
+		initMinvalue();
+	}
+
+	/** @return whether we have a next smallest number */
+	bool hasNext() {
+		if (minValue.empty() && path.empty())
+			return false;
+		else
+			return true;
+
+	}
+
+	/** @return the next smallest number */
+	int next() {
+		if (!minValue.empty())
+		{
+			int result = minValue.front();
+			minValue.pop();
+			return result;
+		}
+		else
+		{
+			TreeNode* t = path.top();
+			path.pop();
+			minValue.push(t->val);
+			if (t->right!=nullptr)
+				rightTreeValue(t->right);
+			return next();
+		}
+	}
+};
+
+int main()
+{
+	TreeNode* root=new TreeNode(2);
+	TreeNode* t1 = new TreeNode(1);
+	TreeNode* t2 = new TreeNode(3);
+	root->right = t2;
+	root->left = t1;
+	BSTIterator bs(root);
+	while (bs.hasNext())
+		cout << bs.next() << endl;
+	return 0;
+}
